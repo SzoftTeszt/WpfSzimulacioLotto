5c3dadeff71b5cde1f5ce92ca5f03099e02a852f
Nagy János <nagy@zold.lan>
2024-06-03 14:13:22 +0200

    Add project files.

---
+++ WpfSzimulacioLotto/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSzimulacioLotto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DrawBetBox();
+
+        }
+
+        private void DrawBetBox() {
+            for (int i = 1; i < 91; i++)
+            {
+                CheckBox box = new CheckBox();
+                box.Content = i.ToString();
+                box.Style = (Style)Application.Current.Resources["BetBoxStyle"]; ;
+                BetBox.Children.Add(box);
+
+            }
+        }
+
+
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
